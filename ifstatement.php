c15f3c19cf5df101411253251052c32497eb95ef
Jaypee924 <mirandajaypee_bsit@plmun.edu.ph>
2025-08-28 17:06:37 +0800

    Update ifstatement.php

--- ifstatement.php
+++ ifstatement.php
@@ -1,1 +1,10 @@
+<?php
+//Miranda, Jaypee N.
+$age = 16;
 
+if ($age >= 18) {
+    echo "You're old enough to vote in Election!";
+} else {
+    echo "I'm Sorry, you're not old enough to vote.";
+}
+?>
