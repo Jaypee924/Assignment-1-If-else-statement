2c878c51ac41998e1251ad063bf42a557ba00e63
Jaypee924 <mirandajaypee_bsit@plmun.edu.ph>
2025-08-28 17:08:29 +0800

    Update if.elseif.else.php

--- if.elseif.else.php
+++ if.elseif.else.php
@@ -1,1 +1,14 @@
+<?php
+//Miranda, Jaypee N.
+$age = 20;
 
+if ($age < 13) {
+    echo "You're a child!";
+} elseif ($age < 20) {
+    echo "You're a teenager!";
+} elseif ($age < 60) {
+    echo "You're an adult!";
+} else {
+    echo "You're a senior!";
+}
+?>
