b55526b6bdf94533572419d2823e5a74259ff373
Jaypee924 <mirandajaypee_bsit@plmun.edu.ph>
2025-08-28 17:08:47 +0800

    Update switchstatement.php

--- switchstatement.php
+++ switchstatement.php
@@ -1,1 +1,19 @@
+<?php
+//Miranda, Jaypee N.
+$score = 90;
 
+switch (true) {
+    case ($score >= 85):
+        echo "Grade: A";
+        break;
+    case ($score >= 65):
+        echo "Grade: B";
+        break;
+    case ($score >= 55):
+        echo "Grade: C";
+        break;
+    default:
+        echo "Grade: F";
+        break;
+}
+?>
